14a863c94e7ee092d59dd54b92f1f7d8e2aaf670
ProishTheIdiot <64977705+thepoopooman652@users.noreply.github.com>
2025-11-20 13:43:11 -0500

    Update data-viewer.tsx

--- client/src/pages/data-viewer.tsx
+++ client/src/pages/data-viewer.tsx
@@ -1,11 +1,11 @@
 //=============================================
-//
-//               Data Viewer Page
-//         Copyright 2025 ProishTheIdiot
-//
-//         Created on: November 18, 2025   
-//               by ProishTheIdiot   
-//
+//=                                           =
+//=              Data Viewer Page             =
+//=        Copyright 2025 ProishTheIdiot      =
+//=                                           =
+//=        Created on: November 18, 2025      =
+//=              by ProishTheIdiot            =
+//=                                           =
 //=============================================
 import { useState, useRef } from "react";
 import { Link } from "wouter";
