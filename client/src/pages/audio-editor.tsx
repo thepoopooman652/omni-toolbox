a98c63fb7a6b13e675fd569e39fdf9efd0788cd6
ProishTheIdiot <64977705+thepoopooman652@users.noreply.github.com>
2025-11-20 12:14:12 -0500

    Update copyright notice/file label

--- client/src/pages/audio-editor.tsx
+++ client/src/pages/audio-editor.tsx
@@ -1,4 +1,4 @@
-//=============================================
+//==============================================
 //
 //               Audio Editor Page
 //         Copyright 2025 ProishTheIdiot
@@ -6,7 +6,7 @@
 //         Created on: November 18, 2025   
 //               by ProishTheIdiot   
 //
-//=============================================
+//==============================================
 import { useState, useRef, useEffect } from "react";
 import { Link } from "wouter";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
